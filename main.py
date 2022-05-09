5bf19c62288ca43692f1759aa1470ee97dab33f9
AntoineD01 <94221570+AntoineD01@users.noreply.github.com>
2022-05-09 10:36:47 +0200

    add some spaces

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ correction_angle = 0
 bullets = []
 enemies = []
 
-sq = Square((0,255,0),200,200,100,100, 10) #Bullets
+sq = Square((0, 255, 0), 200, 200, 100, 100, 10) #Bullets
 
 run = True
 while run:
@@ -43,7 +43,7 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
             # print(x,y)
-            b = Bullet((255,0,0), sq.rect.centerx, sq.rect.centery, 20, 20, 20, x, y)
+            b = Bullet((255, 0, 0), sq.rect.centerx, sq.rect.centery, 20, 20, 20, x, y)
             bullets.append(b)
 
     for b in bullets:
@@ -58,25 +58,25 @@ while run:
     canon_stand = canon_stand_image.get_rect()
     mx, my = pygame.mouse.get_pos()
 
-    if my>600: #Half of the window size
-        my=600
+    if my > 600:  # Half of the window size
+        my = 600
 
     dx, dy = mx - player_rect.centerx, my - player_rect.centery
     angle = math.degrees(math.atan2(-dy, dx)) - correction_angle
     canon = pygame.transform.rotate(canon_shoot, angle)
     rot_image_rect = canon.get_rect(center=(590,610))
     window.fill((255, 255, 255))
-    window.blit(background_sky,(0,0))
+    window.blit(background_sky, (0, 0))
 
-    for x in range(0,1200,64):
-        window.blit(background_grass,(x,660))
-    for y in range(64,800,43):
+    for x in range(0, 1200, 64):
+        window.blit(background_grass, (x, 660))
+    for y in range(64, 800, 43):
         for x in range(0, 1200, 63):
             window.blit(background_ground, (x, 660 + y))
 
 
     window.blit(canon, rot_image_rect)
-    window.blit(canon_stand_image,(550,600))
+    window.blit(canon_stand_image, (550, 600))
 
     for b in bullets:
         b.draw(window)
