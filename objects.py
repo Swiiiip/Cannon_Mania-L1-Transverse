f962fbfed28e3fe02c5033d90570a8b7640d3d77
Joseph <joseph.renno@hotmail.com>
2022-05-23 16:17:14 +0200

    Update

--- objects.py
+++ objects.py
@@ -2,24 +2,30 @@ import pygame
 import math
 from random import *
 
-class Square:
+class Enemy:
     def __init__(self, color, x, y, width, height, speed):
-        self.rect = pygame.Rect(x,y,width,height)
+        self.rect = pygame.Rect(x, y, width, height)
         self.color = color
-        self.direction = 'E'
         self.speed = speed
 
-    def collided(self, other_rect):
-        #Return True if self collided with other_rect
+    def move(self):
+        self.rect.x -= self.speed
+
+    def collide_with(self, other_rect):
+        # Return True if self collided with other_rect
         return self.rect.colliderect(other_rect)
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, self.rect)
 
 
-class Bullet(Square):
+GRAVITY = 0.3
+
+class Bullet(pygame.sprite.Sprite):
     def __init__(self, color, x, y, width, height, speed, targetx,targety):
-        super().__init__(color, x, y, width, height, speed)
+        super().__init__()
+        
+        self.rect = pygame.Rect(x, y, width, height)
         angle = math.atan2(targety-y, targetx-x) #get angle to target in radians
 
         self.dx = math.cos(angle)*speed
@@ -28,14 +34,16 @@ class Bullet(Square):
         self.y = y
 
 
-
     #Override
     def move(self):
         #self.x and self.y are floats (decimals) so I get more accuracy
         #if I change self.x and y and then convert to an integer for
         #the rectangle.
+        self.dy += GRAVITY
+        
         self.x = self.x + self.dx
         self.y = self.y + self.dy
+
         self.rect.x = int(self.x)
         self.rect.y = int(self.y)
 
@@ -47,7 +55,7 @@ class Bullet(Square):
 '''
 
 # class for all the obstacles
-class Ennemy(pygame.sprite.Sprite):
+class Enemy(pygame.sprite.Sprite):
     def __init__(self, image, game):
         super().__init__()
 
@@ -70,7 +78,7 @@ class Ennemy(pygame.sprite.Sprite):
             self.game.obstacles.pop(self.game.obstacles.index(self))
 
 
-class EnnemyFalling(Ennemy):
+class EnemyFalling(Enemy):
     def __init__(self, image, game):
         super().__init__(image, game)
         self.rect.x = random.randint(0, SCREEN_SIZE[0])
@@ -90,7 +98,7 @@ class EnnemyFalling(Ennemy):
             self.game.obstacles.pop(self.game.obstacles.index(self))
 
 
-class Cat(Ennemy):
+class Cat(Enemy):
     N_CATS = 3
     CATS = [
         pygame.transform.scale(
