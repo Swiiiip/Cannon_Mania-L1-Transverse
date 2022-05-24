57a71f9bb2f4cd19a48eb6266bd0715f0e223abd
Max <60543092+MaxxBzt@users.noreply.github.com>
2022-05-24 09:52:33 +0200

    Joe modifications + End screen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import pygame
+
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -21,7 +23,7 @@ window = pygame.display.set_mode((window_width, window_height))
 
 FPS = 60
 
-   
+
 #initialize images (canon (divided in the shooting part and the stand), the sky and the ground)
 canon_shoot = pygame.transform.smoothscale(pygame.image.load("cannon.png").convert_alpha(), (130, 47))
 canon_stand_image = pygame.transform.smoothscale(pygame.image.load("cannon2.png").convert_alpha(),(66,48))
@@ -38,7 +40,7 @@ exit_button = pygame.transform.smoothscale(pygame.image.load("Exit_button.png").
 
 castle_start = pygame.transform.smoothscale(pygame.image.load("castle_start.png").convert_alpha(), (700, 454))
 
-#For the collision of enemies with the canon 
+#For the collision of enemies with the canon
 canon_stand = canon_stand_image.get_rect()
 player_rect = canon_shoot.get_rect(center=(215, 625))
 
@@ -46,6 +48,7 @@ player_rect = canon_shoot.get_rect(center=(215, 625))
 #  90 - image is looking up
 # 180 - image is looking to the left
 # 270 - image is looking down
+
 correction_angle = 0
 
 
@@ -61,12 +64,12 @@ def start_menu():
         # Loop to draw the grass because the size of it is small, so we draw it multiple times on the same y, but changing the x
         for x in range(0,1200,64):
             window.blit(background_grass,(x,660))
-        
+
         # Loop to draw the ground because the size of it is small, so we draw it multiple times on tdifferent y's and x's
         for y in range(64,800,43):
             for x in range(0, 1200, 63):
                 window.blit(background_ground, (x, 660 + y))
-        
+
         window.blit(start_button, (window_width/2 - 110, 200))
         window.blit(exit_button, (window_width/2- 110, 300))
         window.blit(castle_start, (window_width/2 - 350, 210))
@@ -77,14 +80,12 @@ def start_menu():
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 run = False
-        
+
         pygame.display.flip()
-               
-                
+
+
 
 def game_function():
-
-    
     #correction_angle = 0
 
     #initialize the objects
@@ -103,22 +104,15 @@ def game_function():
     max_life = 100
     life = max_life
 
-    
+
 
     clock = pygame.time.Clock()
 
     run = True
     while run:
 
-
-
         pygame.mouse.set_visible(False)#Make the mouse invisible
 
-        
-
-
-        
-        
         # Sky Background (Actually drawing it)
         window.fill((255, 255, 255))
         window.blit(background_sky, (0, 0))
@@ -127,25 +121,25 @@ def game_function():
         # Loop to draw the grass because the size of it is small, so we draw it multiple times on the same y, but changing the x
         for x in range(0,1200,64):
             window.blit(background_grass,(x,660))
-        
+
         # Loop to draw the ground because the size of it is small, so we draw it multiple times on tdifferent y's and x's
         for y in range(64,800,43):
             for x in range(0, 1200, 63):
                 window.blit(background_ground, (x, 660 + y))
-        
+
 
 
         mx, my = pygame.mouse.get_pos() #Positions of the mouse
 
         #Half of the window size
 
-        if my > 620: 
+        if my > 620:
             my = 620
         if mx < 200:
             mx = 200
-        
+
         #Rotation of the canon
-        dx, dy = mx - player_rect.centerx, my - player_rect.centery 
+        dx, dy = mx - player_rect.centerx, my - player_rect.centery
         angle = math.degrees(math.atan2(-dy, dx)) - correction_angle
         canon = pygame.transform.rotate(canon_shoot, angle)
         rot_image_rect = canon.get_rect(center=(215, 625))
@@ -153,8 +147,8 @@ def game_function():
         window.blit(canon, rot_image_rect)
         window.blit(canon_stand_image,(165,615))
 
-    
-    
+
+
         # display life
         pygame.draw.rect(window, (100, 0, 0), (10, 100, max_life, 10))
         pygame.draw.rect(window, (255, 0, 0), (10, 100, life, 10))
@@ -162,14 +156,7 @@ def game_function():
         if not enemy_spawn_cooldown:
             if random.random() < enemy_spawn_probability:
                 enemy_spawn_cooldown = True
-                '''enemies.append(EnemyFalling(ARROW, game))
-                enemies.append(EnemyFalling(ARROW, game))
-                enemies.append(EnemyFalling(ARROW, game))
-                enemies.append(EnemyFalling(ARROW, game))
-                if random.random() < 0.5:
-                    enemies.append(Cat(game))
-                else:
-                    enemies.append(Rock(game))'''
+
                 width = random.randint(100, 200)
                 height = random.randint(100, 200)
 
@@ -190,12 +177,18 @@ def game_function():
             if (rot_image_rect.colliderect(enemy.rect) or enemy.rect.x <= -enemy.rect.width):
                 enemies.remove(enemy)
                 life -= 10
-            
+
             else:
                 enemy.draw(window)
 
         if life <= 0:
-            run = False
+            window.fill((127, 174, 226))
+            game_over_text = pygame.font.SysFont(None, 80).render("You lose !", True, (255, 255, 255))
+            window.blit(game_over_text, (470, 150))
+            mouse = pygame.mouse.get_pos()
+            window.blit(game_over_text, (800 / 2 + 50, 1200 / 2))
+            #run=False
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -229,52 +222,13 @@ def game_function():
         pygame.display.flip()
         clock.tick(FPS)
 
-    
 
 
-    '''
 
-    enemy_spawn_cooldown = False
-    enemy_spawn_cooldown_counter = 0
-    enemy_spawn_cooldown = SPAWN_PROBABILITY_PER_SECOND / FPS
-
-    for obstacle in enemies:
-        obstacle.update()
-
-    for projectile in game.projectiles:
-        projectile.update()
-        for obstacle in enemies:
-            if isinstance(obstacle, EnemyFalling) and projectile.rect.colliderect(obstacle.rect):
-                enemies.remove(obstacle)
-                game.projectiles.remove(projectile)
-
-    if not enemy_spawn_cooldown:
-        if random.random() < enemy_spawn_cooldown:
-            enemy_spawn_cooldown = True
-            enemies.append(EnemyFalling(ARROW, game))
-            enemies.append(EnemyFalling(ARROW, game))
-            enemies.append(EnemyFalling(ARROW, game))
-            enemies.append(EnemyFalling(ARROW, game))
-            if random.random() < 0.5:
-                enemies.append(Cat(game))
-            else:
-                enemies.append(Rock(game))
-
-    # Spawn cooldown
-    else:
-        enemy_spawn_cooldown_counter += clock.get_time()
-        if enemy_spawn_cooldown_counter >= SPAWN_COOLDOWN:
-            enemy_spawn_cooldown = False
-            enemy_spawn_cooldown_counter = 0
-
-    # Apply obstacles
-        for obstacle in enemies:
-            screen.blit(obstacle.image, obstacle.rect)
-
-    '''
 
     pygame.quit()
     exit()
+
 start_menu()
 game_function()
 
